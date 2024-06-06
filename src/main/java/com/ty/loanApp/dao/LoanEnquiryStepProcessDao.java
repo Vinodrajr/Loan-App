67ebc08e55974c4bee1b88e1ca81cd83aa9c7629
unknown <deekshamanoj545@gmail.com>
2024-06-06 10:58:31 +0530

    Changed Loan enquiry process dao layer

--- src/main/java/com/ty/loanApp/dao/LoanEnquiryStepProcessDao.java
+++ src/main/java/com/ty/loanApp/dao/LoanEnquiryStepProcessDao.java
@@ -5,4 +5,12 @@ import com.ty.loanApp.entity.LoanEnquiryStepProcess;
 public interface LoanEnquiryStepProcessDao {
 
 	LoanEnquiryStepProcess saveEnquiryStepProcess(LoanEnquiryStepProcess loanEnquiryStepProcess);
+	
+	LoanEnquiryStepProcess updateEnquiryStepProcess(int processId, LoanEnquiryStepProcess loanEnquiryStepProcess);
+	
+	String deleteEnquiryStepProcess(int processId);
+	
+	LoanEnquiryStepProcess getEnquiryStepProcess(int processId);
+	
+	
 }
--- src/main/java/com/ty/loanApp/dao/implementation/LoanEnquiryStepProcessDaoImplementation.java
+++ src/main/java/com/ty/loanApp/dao/implementation/LoanEnquiryStepProcessDaoImplementation.java
@@ -1,7 +1,10 @@
 package com.ty.loanApp.dao.implementation;
 
+import java.util.Optional;
+
 import com.ty.loanApp.dao.LoanEnquiryStepProcessDao;
 import com.ty.loanApp.entity.LoanEnquiryStepProcess;
+import com.ty.loanApp.exception.LoanEnquiryProcessIdNotFound;
 import com.ty.loanApp.repository.LoanEnquiryStepProcessRepository;
 
 public class LoanEnquiryStepProcessDaoImplementation implements LoanEnquiryStepProcessDao {
@@ -15,6 +18,38 @@ public class LoanEnquiryStepProcessDaoImplementation implements LoanEnquiryStepP
 	@Override
 	public LoanEnquiryStepProcess saveEnquiryStepProcess(LoanEnquiryStepProcess loanEnquiryStepProcess) {
 		return loanEnquiryStepProcessRepository.save(loanEnquiryStepProcess);
+	}
+
+	@Override
+	public LoanEnquiryStepProcess updateEnquiryStepProcess(int processId, LoanEnquiryStepProcess loanEnquiryStepProcess) {
+		Optional<LoanEnquiryStepProcess> op = loanEnquiryStepProcessRepository.findById(processId);
+		if(op.isPresent())
+		{
+			LoanEnquiryStepProcess enquiryStepProcess = op.get();
+			loanEnquiryStepProcess.setProcessId(enquiryStepProcess.getProcessId());
+			loanEnquiryStepProcessRepository.save(loanEnquiryStepProcess);
+		}
+		throw new LoanEnquiryProcessIdNotFound("Loan Enquiry process ID Not Found");
+	}
+
+	@Override
+	public String deleteEnquiryStepProcess(int processId) {
+		Optional<LoanEnquiryStepProcess> op = loanEnquiryStepProcessRepository.findById(processId);
+		if(op.isPresent())
+		{
+			loanEnquiryStepProcessRepository.delete(op.get());
+		}
+		throw new LoanEnquiryProcessIdNotFound("Loan Enquiry process ID Not Found");
+	}
+
+	@Override
+	public LoanEnquiryStepProcess getEnquiryStepProcess(int processId) {
+		Optional<LoanEnquiryStepProcess> op = loanEnquiryStepProcessRepository.findById(processId);
+		if(op.isPresent())
+		{
+			return op.get();
+		}
+		throw new LoanEnquiryProcessIdNotFound("Loan Enquiry process ID Not Found");
 	}
 
 }
