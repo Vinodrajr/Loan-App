d5d9b876e97bc7a1bc3696756c7d96bd4300ae66
unknown <vinod.r@testyantra.com>
2024-06-06 12:33:18 +0530

    validations added in loanenquirystepprocess

--- src/main/java/com/ty/loanApp/entity/LoanEnquiryStepProcess.java
+++ src/main/java/com/ty/loanApp/entity/LoanEnquiryStepProcess.java
@@ -1,15 +1,19 @@
 package com.ty.loanApp.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 @Entity
 public class LoanEnquiryStepProcess {
-	
-	private int processId ;
-	private int stepCount ;
-	private boolean isCompleted ;
-	private String accountNumber ;
+
+	private int processId;
+	@Min(1)
+	@Max(5)
+	private int stepCount;
+	private boolean isCompleted;
+	private String accountNumber;
 
 }
