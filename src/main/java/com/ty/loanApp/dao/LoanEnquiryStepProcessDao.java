70532279dc11fc55b46023ae74f1be99c124cb85
unknown <vinod.r@testyantra.com>
2024-06-06 10:17:48 +0530

    loanenquirystepprocess dao and repository created

---
+++ src/main/java/com/ty/loanApp/dao/LoanEnquiryStepProcessDao.java
@@ -0,0 +1,5 @@
+package com.ty.loanApp.dao;
+
+public interface LoanEnquiryStepProcessDao {
+
+}
---
+++ src/main/java/com/ty/loanApp/dao/implementation/LoanEnquiryStepProcessDaoImplementation.java
@@ -0,0 +1,7 @@
+package com.ty.loanApp.dao.implementation;
+
+import com.ty.loanApp.dao.LoanEnquiryStepProcessDao;
+
+public class LoanEnquiryStepProcessDaoImplementation implements LoanEnquiryStepProcessDao {
+
+}
